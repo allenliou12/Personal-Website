4e13b67b07bf610e2aea9c226c7e8897199072a2
allenliou12 <allenliou12@gmail.com>
2024-12-18 09:06:31 +0800

    Restore functionality of hero section button

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ particlesJS("particles-js", {
     }
   },
   interactivity: {
-    detect_on: "window",
+    detect_on: "canvas",
     events: {
       onhover: { enable: true, mode: "repulse" },
       onclick: { enable: false, mode: "push" },
@@ -53,20 +53,3 @@ particlesJS("particles-js", {
   },
   retina_detect: false
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function () {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
